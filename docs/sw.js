a260d8c73fab10b4255feaf0995ffa3b8e46fb02
alfredfaeric <alfredfaeric@gmail.com>
2026-02-21 17:43:33 +0000

    Add docs folder with game files

---
+++ docs/sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'mini-bac-v1';
+const urlsToCache = [
+  '/mini-bac/',
+  '/mini-bac/index.html',
+  '/mini-bac/manifest.json',
+  '/mini-bac/icon.svg'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request).then(fetchResponse => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        });
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
